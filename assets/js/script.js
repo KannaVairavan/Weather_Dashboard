9be534bd1034e9020ae3b9926e1ee01c076a706d
Kannathal Vairavan <kannamanickam@gmail.com>
2021-01-29 22:20:39 -0600

    Added UV index API and added style class based UV index number

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,18 @@
 
+var searchBtnEl=document.getElementById("search-button");
+var inputCityEl=document.getElementById("search-city");
+var TemperatureEl=document.getElementById("temp");
+var humidityEl=document.getElementById("humidity");
+var windSpeedEl=document.getElementById("wind-speed");
+var uvIndexEl=document.getElementById("uv-index");
+var currentCityEl=document.getElementById("current-city");
+var styleUV=document.querySelector(".style-uv");
 
-
-
+console.log(inputCityEl.value.trim());
+   
+var key='21fcbf51cee9b6b8bc09bd26d3ff8386'
 function getApi(cityID) {
-    var key='21fcbf51cee9b6b8bc09bd26d3ff8386'
+    
     var requestUrl =  'https://api.openweathermap.org/data/2.5/weather?q=' + cityID + '&appid='+ key
     console.log(requestUrl);
     fetch(requestUrl)
@@ -11,16 +20,16 @@ function getApi(cityID) {
         return response.json();
        })
       .then(function (data) {
-        // console.log to examine the data
         
+        // check status
 
-        
+        // console.log to examine the data
         console.log(data);
         var cityNameValue=data.name;
         var tempValue=data.main.temp;
         var temperatureFahrenheit = (tempValue - 273.15) * 1.8 +32;
-        var humidityValue=data.main.humidity;
-        var windSpeedValue=data.wind.speed;
+        
+        var windSpeedValue=(data.wind.speed) * 2.237;
         var iconValue=data.weather[0].icon;
         console.log("icon ", iconValue); 
         var dtValue=data.dt;
@@ -28,9 +37,71 @@ function getApi(cityID) {
         console.log("date ", displaydate);
         console.log("temp ", temperatureFahrenheit);
         // icon is rendering from https://openweathermap.org/weather-conditions#How-to-get-icon-URL
-        iconURL='http://openweathermap.org/img/wn/' + iconValue +'@2x.png'
-        console.log("icon ", iconURL);            
+        iconURL="https://openweathermap.org/img/wn/"+iconValue +"@2x.png";
+        // "http://openweathermap.org/img/wn/" + iconValue +"@2x.png"
+        console.log("icon ", iconURL);    
+        var imgEl="<img src= " + iconURL + ">"
+        console.log(imgEl);
+        currentCityEl.textContent=cityNameValue + " (" + displaydate + ")" /*+ <img src="https://openweathermap.org/img/wn/" + iconValue + "@2x.png">;*/
+        console.log(cityNameValue);
+        TemperatureEl.innerText=temperatureFahrenheit.toFixed(2);
+        humidityEl.innerText=data.main.humidity;
+        windSpeedEl.innerText=windSpeedValue.toFixed(1) + " MPH";
+        console.log(data.coord.lat)
+        uvindex(data.coord.lat, data.coord.lon);
+       
       });
   }
-  getApi("houston");
+  
 
+  searchBtnEl.addEventListener("click", function(){
+    console.log(inputCityEl.value.trim())
+    if (inputCityEl.value.trim() !==""){
+      
+      // render weather info
+      getApi(inputCityEl.value.trim());
+
+    } else {
+      alert ("Enter city name")
+    }
+    
+  })
+
+  function uvindex(lat, lon){
+    var uvindexurl='http://api.openweathermap.org/data/2.5/uvi?lat=' + lat + '&lon=' + lon + '&appid=' + key
+    fetch(uvindexurl)
+    .then(function (response) {
+      return response.json();
+     })
+    .then(function (data) {
+      uvIndexEl.innerText=data.value;
+      // apply class based on uv index
+      if(data.value >=0 && data.value < 3){
+        styleUV.classList.add ("favorable");
+      }else if(data.value >= 3 && data.value < 6 ){
+        styleUV.classList.add ("moderate");
+      }else{
+        styleUV.classList.add ("severe");
+      }
+      return data.value
+      console.log("uv ", data)
+
+    });
+  }
+
+  function forecast(cityID){
+    var forecasturl='api.openweathermap.org/data/2.5/forecast?q=' + cityID + '&appid='+ key
+    
+    fetch(uvindexurl)
+    .then(function (response) {
+      return response.json();
+     })
+    .then(function (data) {
+      
+      console.log("forecast ", data)
+
+      // loop through 5 day forecast
+
+
+    });
+  }
